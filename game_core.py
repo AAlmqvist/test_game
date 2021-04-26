b3deb15c9e6a92429f1ee42fc7dc06fd45887f2e
Anton Almqvist <jantonalmqvist@gmail.com>
2021-04-26 11:50:27 +0200

    First commit - running game moving chars

---
+++ game_core.py
@@ -0,0 +1,209 @@
+import pyglet
+from pyglet.window.key import *
+import os
+
+#Create the window
+window = pyglet.window.Window()
+
+# creating path to load image/sound from right directories
+work_dir = os.path.dirname(os.path.relpath(__file__))
+pyglet.resource.path = [os.path.join(work_dir, './images'), os.path.join(work_dir, './sounds')]
+pyglet.resource.reindex()
+
+# create batches for moving and static objects
+batch = pyglet.graphics.Batch()
+static_batch = pyglet.graphics.Batch()
+
+# label to display text on how to get help 
+test_label = pyglet.text.Label(anchor_x='left', anchor_y='top')
+
+# label with actual help text
+help_label = pyglet.text.Label(anchor_x='center', anchor_y='center')
+help_draw = False
+
+# Function for when window draws the new state
+@window.event
+def on_draw():
+    pyglet.gl.glClearColor(*background, 1)
+    window.clear()
+    static_batch.draw()
+    batch.draw()
+
+    test_label.text = 'Help: H'
+    test_label.font_size = window.height//40
+    test_label.draw()
+
+    if help_draw:
+        help_label.text = 'Red blob: WASD\nGreen blob: SPACE to jump and O-P to move sideways'
+        help_label.font_size = window.height//30
+        help_label.width = window.width//1.5
+        help_label.height = window.height//1.5
+        help_label.multiline = True
+        help_label.draw()
+
+# sets to save state of key-presses
+key_state = key_state_old = set()
+
+# when key is pressed
+@window.event
+def on_key_press(symbol, modifiers):
+    key_state.add(symbol)
+
+# when key is released
+@window.event
+def on_key_release(symbol, modifiers):
+    key_state.discard(symbol)
+
+# boolean funtction to see if key is pressed
+def key(k):
+    return k in key_state
+
+# boolean function to see if key was pressed last cycle
+def key_old(k):
+    return k in key_state_old
+
+# list to bunch all the moving stuff together 
+movers = []
+
+# dummy class for all moving objects
+class Mover:
+    pass
+
+# creating a moving object
+def createMovingObject(move_func, image=None, size=0.1, x=0, y=0, dx=0, dy=0, **kargs):
+    m = Mover()
+    # This is the move-function that specifies each moving objects abilities
+    m.move = move_func
+    m.image = image
+
+    if image:
+        m.sprite = pyglet.sprite.Sprite(image, batch=batch)
+        m.sprite.scale_x = m.sprite.scale_y = 0
+    else:
+        m.sprite = None
+    
+    m.sx = m.sy = size
+    m.x, m.y = x, y
+    m.dx, m.dy = dx, dy
+    m.r = m.state = m.time = 0
+    m.life = 1
+
+    for k, v in kargs.items():
+        setattr(m, k, v)
+
+    movers.append(m)
+
+    return m
+
+# Gridded list that keeps track of where on the map there are walls
+walls = [[None for loop_y in range(20)] for loop_x in range(30)]
+
+# dummy class for static objects
+class StaticObject:
+    pass
+
+# Function to create a static object, a.k.a. a wall
+def createStaticObject(image=None, size=0.1, x=0, y=0, **kargs):
+    st = StaticObject()
+    st.image = image
+
+    if image:
+        st.sprite = pyglet.sprite.Sprite(image, batch=static_batch)
+        st.scale_x = st.scale_y = 0
+        # 900x600 window, 30x20 grid cells, each grid cell is 30x30 pixels
+        st.sprite.x = 15 + x*30 # +15 to center the sprite
+        st.sprite.y = 15 + y*30 # +15 to center the sprite
+    else:
+        st.sprite = None
+
+    st.x = (st.sprite.x - 450) / 450
+    st.y = (st.sprite.y - 300) / 300
+
+    for k, v in kargs.items():
+        setattr(st, k, v)
+    walls[x][y] = st
+
+# Function that loads the image files
+def image(file):
+    img = pyglet.resource.image(file)
+    img.anchor_x = img.width//2
+    img.anchor_y = img.height//2
+    return img
+
+# Function to get itterator from group of objects with specified move-function (could list many move funcs)
+def group(*move):
+    for m in movers:
+        if m.move in move:
+            yield m
+
+# time constants used for when to move stuff
+time_sum = 0
+time_min = (1/60)*0.9
+
+# Cyclic function called by the window to update the state of the game
+def move_objects(dt):
+    global help_draw, key_state_old, time_sum, time_min, movers
+
+    time_sum += dt
+
+    if time_sum > time_min:
+        time_sum = 0
+
+        for m in movers:
+            m.move(m)
+    
+    w, w2, h2 = window.width, window.width//2, window.height//2
+
+    for m in movers:
+        if m.sprite:
+            m.sprite.image = m.image
+            m.sprite.scale_x = m.sx * w / m.image.width
+            m.sprite.scale_y = m.sy *w / m.image.height
+            m.sprite.x = m.x * w2 + w2
+            m.sprite.y = m.y * h2 + h2
+            m.sprite.rotation = -m.r * 360
+
+    old_movers = movers
+    movers = [m for m in movers if m.life > 0]
+
+    if key(ESCAPE):
+        pyglet.app.exit()
+
+    if key(H) and not key_old(H):
+        help_draw = not help_draw
+
+    if key(BACKSPACE) and not key_old(BACKSPACE):
+        movers.clear()
+        start()
+
+    key_state_old = key_state.copy()
+
+# Function that starts and runs the app
+def run(start_func, w=window.width, h=window.height, bg=(1,1,1), fs=False, tc=(0.5, 0.5, 0.5), tfn='Arial'):
+    global start, background
+
+    
+    window.set_size(w,h)
+    window.set_fullscreen(fs)
+
+
+
+    start = start_func
+
+    background = bg
+    
+
+    test_label.color = int(255*tc[0]), int(255*tc[1]), int(255*tc[2]), 255
+    test_label.font_name = tfn
+    test_label.x = 32
+    test_label.y = h-32
+
+    help_label.x = w//2
+    help_label.y = h//2
+
+
+    start()
+
+    pyglet.clock.schedule(move_objects)
+    
+    pyglet.app.run()
---
+++ main.py
@@ -0,0 +1,170 @@
+import pyglet
+from game_core import *
+import math
+
+# Sprites: around 30x30 pixels, Screen is 900x600
+
+w2 = 900//2
+h2 = 600//2
+
+# Loading images
+player_image = image('weird_guy.png')
+jumper_image = image('jump_guy.png')
+badmouse = image('badmouse.png')
+brick_image = image('brick.png')
+spikes_image = image('spikes.png')
+
+# Defining the movement of normal player
+def player(p):
+    v = 0.02
+
+    if key( pyglet.window.key.A ):
+        p.dx -= v
+    
+    if key( pyglet.window.key.W ):
+        p.dy += v
+    
+    if key( pyglet.window.key.D ):
+        p.dx += v
+    
+    if key(pyglet.window.key.S):
+        p.dy -= v
+
+    #Check for collision
+    x_grid = max(1, min(math.floor((p.x+1)*15),28))
+    y_grid = max(1, min(math.floor((p.y+1)*10),18))
+
+    if abs(p.dy) > 0:
+        sign = round(p.dy/abs(p.dy))
+        for j in range(-1,2):
+            potential_wall = walls[x_grid+j][y_grid+sign]
+            if potential_wall:
+                if abs(p.y+p.dy - potential_wall.y) < 1.5*p.sy + 15/300 and abs(p.x - potential_wall.x) < 0.8*p.sx + 15/450:
+                    p.y = potential_wall.y - sign*(1.5*p.sy + 15/300)
+                    p.dy = 0
+
+    if abs(p.dx) > 0:
+        sign = round(p.dx/abs(p.dx))
+        for j in range(-1,2):
+            potential_wall = walls[x_grid+sign][y_grid+j]
+            if potential_wall:
+                if abs(p.x+p.dx - potential_wall.x) < p.sx + 15/450 and abs(p.y - potential_wall.y) < p.sy + 15/300:
+                    p.x = potential_wall.x - sign*(p.sx + 15/450)
+                    p.dx = 0
+    
+    if math.sqrt(p.dx**2+p.dy**2) > v:
+        p.dx = p.dx/1.42
+        p.dy = p.dy/1.42
+    
+    p.x += p.dx
+    p.y += p.dy
+
+    p.dx, p.dy = 0, 0
+    p.time += time_min
+
+# defining the movement of a character affected by gravity
+def jumper(l):
+    # gravitational constant
+    g = 0.0012
+    l.time += time_min
+    
+    if l.y > -1 + 2*l.sy:
+        l.dy -= g
+
+    if key(pyglet.window.key.SPACE) and l.time > 0.1 and l.ground_below:
+        l.dy = 0.04
+        l.time = 0
+    
+    if key(pyglet.window.key.P):
+        l.dx += 0.02
+    
+    if key(pyglet.window.key.O):
+        l.dx -= 0.02
+
+    # rotate sprite according to velocity in x-direction
+    if l.dx > 0:
+        l.r = 1-0.125
+    elif l.dx <0:
+        l.r = 0.125
+    else:
+        l.r = 0
+
+    #Check for collision
+    x_grid = max(1, min(math.floor((l.x+1)*15),28))
+    y_grid = max(1, min(math.floor((l.y+1)*10),18))
+
+    solid_ground = False
+    if abs(l.dy) > 0:
+        sign = round(l.dy/abs(l.dy))
+        for j in range(-1,2):
+            potential_wall = walls[x_grid+j][y_grid+sign]
+            if potential_wall:
+                if abs(l.y+l.dy - potential_wall.y) < 1.5*l.sy + 15/300 and abs(l.x - potential_wall.x) < 0.8*l.sx + 15/450:
+                    if l.dy < 0:
+                        solid_ground = True
+                    l.y = potential_wall.y - sign*(1.5*l.sy + 15/300)
+                    l.dy = 0
+
+    if abs(l.dx) > 0:
+        sign = round(l.dx/abs(l.dx))
+        for j in range(-1,2):
+            potential_wall = walls[x_grid+sign][y_grid+j]
+            if potential_wall:
+                if abs(l.x+l.dx - potential_wall.x) < l.sx + 15/450 and abs(l.y - potential_wall.y) < l.sy + 15/300:
+                    l.x = potential_wall.x - sign*(l.sx + 15/450)
+                    l.dx = 0
+    l.y += l.dy
+    l.x += l.dx
+    l.ground_below = solid_ground
+    l.dx = 0
+
+def spikes(sp):
+
+    sp.time += time_min
+
+def createBorders():
+    for i in range(30):
+        createStaticObject(brick_image, 30/600, i, 0)
+        createStaticObject(brick_image, 30/600, i, 19)
+    for j in range(20):
+        createStaticObject(brick_image, 30/600, 0, j)
+        createStaticObject(brick_image, 30/600, 29, j)
+
+def level(ind):
+    if 1:
+        for i in range(4,10):
+            createStaticObject(brick_image, 30/600, i, 4)
+        for i in range(3,8):
+            createStaticObject(brick_image, 30/600, i, 15)
+        for i in range(16,23):
+            createStaticObject(brick_image, 30/600, i, 8)
+        for i in range(23,26):
+            createStaticObject(brick_image, 30/600, i, 13)
+        for i in range(9,11):
+            createStaticObject(brick_image, 30/600, i, 12)
+        for i in range(17,19):
+            createStaticObject(brick_image, 30/600, i, 14)
+        for i in range(14,17):
+            createStaticObject(brick_image, 30/600, 23, i)
+        for i in range(26,29):
+            createSpikes(-1 + (i+0.5)*(30/450), -1 + (0.1+12/300) )
+
+def createPlayer(size=0.1, x=0, y=0):
+    createMovingObject(player, player_image, size=size, x=x , y=y, life=2)
+
+def createJumper(size=0.1, x=0, y=0):
+    createMovingObject(jumper, jumper_image, size = size, x=x, y=y, ground_below = False, life=2)
+
+def createSpikes(x=0, y=0):
+    createMovingObject(spikes, spikes_image, 27/900, x=x, y=y)
+
+def start_now():
+    print("Starting game")
+    createBorders()
+    level(1)
+    createPlayer(30/900, -0.2 , 0)
+    createJumper(30/900, 0.2, 0)
+
+
+run(start_now, w=2*w2, h=2*h2, bg=(0.5, 0.5, 1), tc=(1,1,1))
+
